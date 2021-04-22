801f7967b6fc10ffbe875e01ffa3ee0a6f53b194
alexandrafossum <fossum.alexandra@gmail.com>
2021-04-22 16:13:47 -0500

    desktop version of block is ready with the exception of fonts and other assets. mobile version is coming along

--- src/components/BlockGrid/index.js
+++ src/components/BlockGrid/index.js
@@ -32,27 +32,29 @@ const BlockGrid = () => {
     
     return(
         <Container maxWidth="container.md">
-            <Grid templateColumns="repeat(2, 1fr)" gap={25}>
+            <Grid className="block-grid" templateColumns={["repeat(2, 1fr)"]} gap={25}>
                 <Box className="block-grid-item">
-                    <BackgroundImage className="block-grid-image" Tag="section" {...bgImage} preserveStackingContext style={{ minHeight: `300px` }} />
+                    <BackgroundImage className="block-grid-image" Tag="section" {...bgImage} preserveStackingContext />
                 </Box>
-                <Box className="block-grid-item" style={{ minHeight: `300px` }}>
+                <Box className="block-grid-item">
                     <Text style={{ fontSize: `25px`, color: `#3FA7B6`, paddingBottom: `5px`, textTransform: `uppercase` }}>Create Your Masterpiece</Text>
                     <Text>The Acrylic Pour on Metal Flowers Class is back with more classes and we are adding resin for no extra charge.</Text>
-                    <Center>
+                    <Center my="10">
                     <DiamondButton buttonStyle="btn--primary" buttonSize="btn--small" style={{ textAlign: `center`, margin: `0 auto` }} py="10">Book a Class</DiamondButton>
                     </Center>
                 </Box>
                 <Box className="block-grid-item">
-                    <BackgroundImage className="block-grid-image" Tag="section" {...bgImage} preserveStackingContext style={{ minHeight: `300px` }}>
-                        <DiamondButton 
-                            className="grid-diamond-btn" 
-                            buttonStyle="btn--primary" 
-                            buttonSize="btn--medium"
-                            m="0"
-                            p="0">
-                                Custom Art Kits
-                        </DiamondButton>
+                    <BackgroundImage className="block-grid-image" Tag="section" {...bgImage} preserveStackingContext>
+                        <Center className="center-grid-diamond">
+                            <DiamondButton 
+                                className="grid-diamond-btn" 
+                                buttonStyle="btn--primary" 
+                                buttonSize="btn--medium"
+                                m="0"
+                                p="0">
+                                    Custom Art Kits
+                            </DiamondButton>
+                        </Center>
                     </BackgroundImage>
                 </Box>
                 <Box className="block-grid-item">
@@ -60,14 +62,15 @@ const BlockGrid = () => {
                         className="block-grid-image" 
                         Tag="section" 
                         {...bgImage} 
-                        preserveStackingContext 
-                        style={{ minHeight: `300px` }}>
-
-                        <DiamondButton 
-                        className="grid-diamond-btn" 
-                        buttonStyle="btn--primary" 
-                        buttonSize="btn--medium">Jewelry</DiamondButton>
-
+                        preserveStackingContext>
+                        <Center className="center-grid-diamond">
+                            <DiamondButton 
+                                className="grid-diamond-btn" 
+                                buttonStyle="btn--primary" 
+                                buttonSize="btn--medium">
+                                Jewelry
+                            </DiamondButton>
+                        </Center>
                     </BackgroundImage>
                 </Box>
             </Grid>
