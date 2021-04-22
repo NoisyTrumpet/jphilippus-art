82ad1034c4aec89f8a7009c5fd02f229fd1e336f
alexandrafossum <fossum.alexandra@gmail.com>
2021-04-22 13:24:24 -0500

    images querying successfully, DiamondButton appearing, needs some styling revision to get the GridBlock and DiamondButton to interact smoothly

--- src/components/BlockGrid/index.js
+++ src/components/BlockGrid/index.js
@@ -1,9 +1,9 @@
 import * as React from "react";
-import { Container, Box, Text, Grid } from "@chakra-ui/layout";
+import { Container, Box, Text, Grid, Center } from "@chakra-ui/layout";
 import BackgroundImage from "gatsby-background-image";
 import { getImage } from "gatsby-plugin-image";
 import { graphql, useStaticQuery } from "gatsby";
-import { DiamondButton } from "../DiamondButton/DiamondButton";
+import DiamondButton from "../DiamondButton/DiamondButton"
 import { convertToBgImage } from "gbimage-bridge";
 
 import "./BlockGrid.scss"
@@ -31,26 +31,43 @@ const BlockGrid = () => {
     const bgImage = convertToBgImage(image)
     
     return(
-
         <Container maxWidth="container.md">
             <Grid templateColumns="repeat(2, 1fr)" gap={25}>
                 <Box className="block-grid-item">
-                    <BackgroundImage className="block-grid-image" Tag="section" {...bgImage} preserveStackingContext style={{ minHeight: `250px` }} />
+                    <BackgroundImage className="block-grid-image" Tag="section" {...bgImage} preserveStackingContext style={{ minHeight: `300px` }} />
                 </Box>
-                <Box className="block-grid-item" style={{ minHeight: `250px` }}>
-                    <Text style={{ fontSize: `25px`, color: `#3FA7B6`, paddingBottom: `5px` }}>Create Your Masterpiece</Text>
+                <Box className="block-grid-item" style={{ minHeight: `300px` }}>
+                    <Text style={{ fontSize: `25px`, color: `#3FA7B6`, paddingBottom: `5px`, textTransform: `uppercase` }}>Create Your Masterpiece</Text>
                     <Text>The Acrylic Pour on Metal Flowers Class is back with more classes and we are adding resin for no extra charge.</Text>
-                    {/* <DiamondButton buttonStyle="btn--primary" buttonSize="btn--small" style={{ textAlign: `center` }}>Book a Class</DiamondButton> */}
+                    <Center>
+                    <DiamondButton buttonStyle="btn--primary" buttonSize="btn--small" style={{ textAlign: `center`, margin: `0 auto` }} py="10">Book a Class</DiamondButton>
+                    </Center>
                 </Box>
                 <Box className="block-grid-item">
-                    <BackgroundImage className="block-grid-image" Tag="section" {...bgImage} preserveStackingContext style={{ minHeight: `250px` }}>
-                    {/* <DiamondButton className="grid-diamond-btn" buttonStyle="btn--primary" buttonSize="btn--small">Test</DiamondButton> */}
+                    <BackgroundImage className="block-grid-image" Tag="section" {...bgImage} preserveStackingContext style={{ minHeight: `300px` }}>
+                        <DiamondButton 
+                            className="grid-diamond-btn" 
+                            buttonStyle="btn--primary" 
+                            buttonSize="btn--medium"
+                            m="0"
+                            p="0">
+                                Custom Art Kits
+                        </DiamondButton>
                     </BackgroundImage>
                 </Box>
                 <Box className="block-grid-item">
-                    <BackgroundImage className="block-grid-image" Tag="section" {...bgImage} preserveStackingContext style={{ minHeight: `250px` }}>
-                    <Text className="grid-item-btn" style={{ fontSize: `20px` }}>Test</Text>
-                    {/* <DiamondButton buttonStyle="btn--primary" buttonSize="btn--small">Test</DiamondButton> */}
+                    <BackgroundImage 
+                        className="block-grid-image" 
+                        Tag="section" 
+                        {...bgImage} 
+                        preserveStackingContext 
+                        style={{ minHeight: `300px` }}>
+
+                        <DiamondButton 
+                        className="grid-diamond-btn" 
+                        buttonStyle="btn--primary" 
+                        buttonSize="btn--medium">Jewelry</DiamondButton>
+
                     </BackgroundImage>
                 </Box>
             </Grid>
