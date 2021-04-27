d71112dd2bbd751189d4fe0c8315ccc2ec1297c9
Heather Preiss <heather.preiss@Heather-MacBook-Air.local>
2021-04-27 11:18:53 -0500

    added Google map with Pin to contact page

---
+++ src/images/SVG/Pin.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+function Pin(props) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="39.904" height="54.398" viewBox="0 0 39.904 54.398">
+  <g id="Layer_1" data-name="Layer 1" transform="translate(-506.374 -1097.108)">
+    <path id="Path_52" data-name="Path 52" d="M546.278,1117.041c0,12.839-15.73,30.4-19.258,34.16a.943.943,0,0,1-1.33.058l-.058-.058c-3.509-3.8-19.258-21.533-19.258-34.16a19.952,19.952,0,0,1,39.9,0Z" fill="#3fa7b6"/>
+  </g>
+</svg>
+  )
+}
+
+export default Pin
