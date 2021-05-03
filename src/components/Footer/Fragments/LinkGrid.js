36be1d9c4cda97646be5560442f1f9a5d5bb2e53
Alexandra Fossum <alexandrafossum@Alexandras-MacBook-Pro.local>
2021-05-03 08:35:05 -0500

    Swapped out chakra Link for Link component. Refactored to exclude break tags.

--- src/components/Footer/Fragments/LinkGrid.js
+++ src/components/Footer/Fragments/LinkGrid.js
@@ -1,4 +1,4 @@
-import { Box, SimpleGrid, Stack } from "@chakra-ui/react"
+import { Box, SimpleGrid, Stack, VStack } from "@chakra-ui/react"
 import * as React from "react"
 import { FooterHeading } from "./FooterHeading"
 import Link from "../../link"
@@ -7,23 +7,23 @@ const LinkGrid = props => (
   <SimpleGrid columns={{base: 1, lg: 2}} {...props}>
     <Box minW="140px">
       <FooterHeading mb="4">Menu</FooterHeading>
-      <SimpleGrid columns={{base: 2, lg: 2}}>
-        <Box mr="5">
+      <SimpleGrid columns={{base: 2, lg: 2}} mb={{ base: 4, lg: 0 }}>
+        <VStack align="stretch" spacing={2} mr={{ base: 0, lg: 2 }}>
           <Link to="/about" display="block">About the Artist</Link>
           <Link to="/shop" display="block">Shop</Link>
-        </Box>
-        <Box ml="5" mb={8}>
+        </VStack>
+        <VStack align="stretch" spacing={2} ml={{ base: 0, lg: 2 }}>
           {/* <Link to="/news" display="block">News/Press</Link> */}
           <Link to="/contact" display="block">Contact</Link>
           <Link to="/faq" display="block">FAQ</Link>
-        </Box>
+        </VStack>
       </SimpleGrid>
     </Box>
     <Box minW="140px">
       <FooterHeading mb="4">Contact</FooterHeading>
-      <Stack>
+      <VStack align="stretch" spacing={2}>
         <Link>1846 North Loop<br />1604 W<br/>Suite 104</Link>
-      </Stack>
+      </VStack>
     </Box>
   </SimpleGrid>
 )
