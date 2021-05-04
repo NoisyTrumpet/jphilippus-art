d21f97f450819706b0c13f1ccd6f981b2fdcd1bf
Heather Preiss <heather.preiss@Heather-MacBook-Air.local>
2021-05-04 09:15:28 -0500

    just a bit of formatting

--- src/components/Testimonials/Quotee.js
+++ src/components/Testimonials/Quotee.js
@@ -1,34 +1,39 @@
-import { Box, Container, Img, Text, useColorModeValue } from '@chakra-ui/react'
-import * as React from 'react'
+import { Box, Container, Img, Text, useColorModeValue } from "@chakra-ui/react"
+import * as React from "react"
 
-export const Quotee = (props) => {
+export const Quotee = props => {
   const { QuoteBlock, name, jobTitle, imageSrc, ...boxProps } = props
   return (
     <Container>
       <Box {...boxProps}>
-      <Text
-        color={useColorModeValue('bg')}
+        <Text
+          color={useColorModeValue("bg")}
           fontSize={{
-            base: 'xl',
-            md: '2xl',
+            base: "xl",
+            md: "2xl",
           }}
           fontWeight="medium"
           mt="6"
         >
           {QuoteBlock}
         </Text>
-    </Box>
-    <Box {...boxProps}>
-      <Img src={imageSrc} mx="auto" objectFit="cover" w="16" h="16" rounded="full" />
-      <Box mt="3" color={useColorModeValue('bg')}>
-        <Text as="cite" fontStyle="normal" fontWeight="bold">
-          {name}
-        </Text>
-        <Text fontSize="sm">
-          {jobTitle}
-        </Text>
       </Box>
-    </Box>
+      <Box {...boxProps}>
+        <Img
+          src={imageSrc}
+          mx="auto"
+          objectFit="cover"
+          w="16"
+          h="16"
+          rounded="full"
+        />
+        <Box mt="3" color={useColorModeValue("bg")}>
+          <Text as="cite" fontStyle="normal" fontWeight="bold">
+            {name}
+          </Text>
+          <Text fontSize="sm">{jobTitle}</Text>
+        </Box>
+      </Box>
     </Container>
   )
 }
