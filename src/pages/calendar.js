a0148f5461ac9f4f28b6f7e31e763e51bc6d6121
Cesar Benavides <cesarbenavides@icloud.com>
2021-06-10 11:04:47 -0500

    testing fullcalendar

--- src/pages/calendar.js
+++ src/pages/calendar.js
@@ -1,20 +1,23 @@
 import { Container } from "@chakra-ui/layout"
 import React from "react"
 import Layout from "../components/Layout/Layout"
+import FullCalendar from "@fullcalendar/react"
+import dayGridPlugin from "@fullcalendar/daygrid"
+import googleCalendarPlugin from "@fullcalendar/google-calendar"
 
 const Calendar = () => {
   return (
     <Layout>
       <Container>
-        <iframe
-          title="J. Philippus Art Studio & Gallery Calendar"
-          src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=2&amp;bgcolor=%233492a1&amp;ctz=America%2FChicago&amp;src=ZmoyMms0bGpiazM1c3BjYjJ2MmJjMzZnMjRAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&amp;color=%23009688&amp;mode=WEEK&amp;showTz=0&amp;showCalendars=0&amp;showTabs=1&amp;showPrint=1&amp;showDate=1&amp;showNav=1&amp;showTitle=1"
-          style={{ borderWidth: "0" }}
-          width="100%"
-          height="600"
-          frameBorder="0"
-          scrolling="no"
-        ></iframe>
+        <FullCalendar
+          plugins={[dayGridPlugin, googleCalendarPlugin]}
+          googleCalendarApiKey={process.env.GATSBY_GOOGLE_MAPS_API_KEY}
+          events={{
+            googleCalendarId: `fj22k4ljbk35spcb2v2bc36g24@group.calendar.google.com`,
+          }}
+          hiddenDays={[0]}
+          initialView="dayGridMonth"
+        />
       </Container>
     </Layout>
   )
