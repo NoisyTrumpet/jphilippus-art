7d41ae41d42431c52684049cab3204c9d741a536
alexandrafossum <fossum.alexandra@gmail.com>
2021-07-09 15:13:06 -0500

    hero image in place on FAQ page. still need copy but have removed dummy text

---
+++ src/constants/faq.js
@@ -0,0 +1,57 @@
+import React from "react"
+export default [
+  {
+    question:
+      "If I go in a group can I do a different class than my friends are doing?",
+    answer:
+      "We prefer you do the same class as the group because it requires step by step teaching for the entire class. We want to make sure we are there throughout the entire process to ensure you enjoy your experience and love your masterpiece when it is complete.",
+  },
+  {
+    question:
+      "If I am not local and cannot pick up my art, can it be shipped to me?",
+    answer: "Yes. We will ship the art to you.",
+  },
+  {
+    question: "Will my artwork be completely ready to take home after class?",
+    answer:
+      "Your artwork is usually ready in 1-2 days. If you need it ready the next day, please let your art teacher know.",
+  },
+  {
+    question: "Can we take food?",
+    answer:
+      "Yes. You can bring food and wine/alcohol and/or drinks. We have a refrigerator and a full counter for you to serve at.",
+  },
+  {
+    question: "What class would be best for kids?",
+    answer:
+      "We have the glass art class that ages 9 up to 109 years and younger than 9 can do the glass art class but we use seaglass and plastic utensils that look like glass. We also have acrylic pours that children can do. Please call the studio to request these classes.",
+  },
+  {
+    question: "Do I have to go in a group?",
+    answer:
+      "No. You can come by yourself or with a couple people or as a group. We also have team buildings.",
+  },
+  {
+    question: "When is the studio open?",
+    answer: "11 am - 6pm, Monday - Saturday.",
+  },
+  {
+    question: "Is the art in the studio for sale?",
+    answer:
+      "Yes, all the art is the work of Jeanne Philippus. She will also do commissioned art.",
+  },
+  {
+    question: "Can I walk in and take a class?",
+    answer:
+      "Most week days we can accommodate walk-in's however on Saturday's we are usually booked so please schedule online or call the studio to see if there is room that day.",
+  },
+  {
+    question:
+      "Can I come along with someone that is a paying customer to the studio to watch and break glass with them?",
+    answer: "We ask that only one person attend with a paying customer.",
+  },
+  {
+    question: "How long are the classes?",
+    answer: "Most classes range from 1 & 1/2 hours to 2 hours.",
+  },
+]
--- src/pages/faq.js
+++ src/pages/faq.js
@@ -1,3 +1,93 @@
-export default function Component() {
-  return "Hello world"
+import React from "react"
+import Layout from "../components/Layout/Layout"
+import { graphql, useStaticQuery } from "gatsby"
+import {
+  Box,
+  Grid,
+  Text,
+  Center,
+  Container,
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  useColorModeValue as mode,
+} from "@chakra-ui/react"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import faqs from "../constants/faq"
+const FAQPage = () => {
+  // Hero Image Query
+  const { heroImage } = useStaticQuery(
+    graphql`
+      query {
+        heroImage: file(relativePath: { eq: "faq-page-hero.jpg" }) {
+          childImageSharp {
+            id
+            gatsbyImageData(
+              width: 1200
+              quality: 60
+              layout: CONSTRAINED
+              placeholder: BLURRED
+              formats: [AUTO, WEBP]
+            )
+          }
+        }
+      }
+    `
+  )
+  // get image data
+  const hero = getImage(heroImage)
+  const bgGray = mode(`bgGray`, `gray.700`)
+  return (
+    <Layout>
+      <Grid
+        templateColumns={["1fr", "1fr", "repeat(2, 1fr)"]}
+        templateRows={["fit-content 1fr", "1fr 1fr", "repeat(1, 1fr)"]}
+      >
+        <Box
+          className="bio-text"
+          px={[4, 4, 4, 12]}
+          py={[8, 8, 8, 12]}
+          style={{ lineHeight: `25px` }}
+          backgroundColor={bgGray}
+        >
+          <Text
+            tag="h1"
+            fontWeight="500"
+            color={`primary`}
+            textTransform="uppercase"
+            fontSize="3xl"
+          >
+            Frequently Asked Questions
+          </Text>
+          <Text py="5">
+            
+          </Text>
+        </Box>
+      <GatsbyImage image={hero} alt="Frequently Asked Questions" />
+      </Grid>
+      {/* FAQ Accordion */}
+      <Container mt={30} mb={40}>
+        <Accordion allowToggle>
+        {faqs.map((faq, index) => (
+          <AccordionItem key={index} style={{ borderBottomColor: `#c09559`, borderTopColor: `#c09559` }}>
+            <h2>
+              <AccordionButton style={{ color: `#c09559`, fontWeight: `500`, fontSize: `20px` }}>
+                <Box flex="1" textAlign="left">
+                  {faq.question}
+                </Box>
+                <AccordionIcon color="#3FA7B6" />
+              </AccordionButton>
+            </h2>
+            <AccordionPanel py={4}>
+              {faq.answer}
+            </AccordionPanel>
+          </AccordionItem>
+          ))}
+        </Accordion>
+      </Container>
+    </Layout>
+  )
 }
+export default FAQPage
