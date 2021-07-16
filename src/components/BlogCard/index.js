a961db110b4c0b6d23a0a90140728b977578a752
Heather Preiss <heather.l.preiss@gmail.com>
2021-07-16 17:53:56 -0500

    created and styled blog/news page

---
+++ src/components/BlogCard/index.js
@@ -0,0 +1,145 @@
+import {
+    Box,
+    Button,
+    Flex,
+    Heading,
+    Img,
+    Link,
+    Text,
+    useColorModeValue,
+} from '@chakra-ui/react'
+import React from 'react'
+import moment from 'moment'
+//   import DiamondButton from "../DiamondButton/DiamondButton"
+
+
+const BlogCard = (props) => {
+    const { title, href, description, author, category, date, blogImage, featured } = props
+    const themeGold = useColorModeValue(`secondary`, `gray.300`)
+    const themeBlue = useColorModeValue(`primary`, `gray.300`)
+    const bg = useColorModeValue(`cardBg`, `dark.cardBg`)
+    if (featured) {
+        return (
+            <Box
+                as="section"
+                bg={{
+                    sm: { bg },
+                }}
+                overflow="hidden"
+            >
+                <Flex direction={["column", "column", "row", "row"]}>
+                    <Link href={href} flex="1.2">
+                        <Img maxHeight="350" w={`100%`} objectFit="cover" alt={title} src={blogImage} />
+                    </Link>
+                    <Flex
+                        direction="column"
+                        flex="1"
+                        justifyContent={`space-between`}
+                        px={{
+                            sm: '6',
+                        }}
+                        py="5"
+                    >
+                        <Box>
+                            <Heading as="h3" size="md" mb="1" lineHeight="base" color={themeGold}>
+                                <Link href={href}>{title}</Link>
+                            </Heading>
+                            <Text
+                                casing="uppercase"
+                                letterSpacing="wider"
+                                fontSize="xs"
+                                fontWeight="semibold"
+                                mb="6"
+                                color={themeBlue}
+                            >
+                                {/* {date} */}
+                                {moment(date).format("MMM Do YY")}
+                            </Text>
+                            <Box mb="3" dangerouslySetInnerHTML={{
+                                __html: description,
+                            }} />
+                        </Box>
+                        <Button
+                            as={`a`}
+                            href={href}
+                            bg={themeBlue}
+                            color={`white`}
+                            maxW={`40%`}
+                            alignSelf={`flex-end`}
+                            _hover={{ bg: `secondary`, }}
+                        >
+                            Read More...
+                        </Button>
+                    </Flex>
+                </Flex>
+            </Box>
+        )
+    }
+    else {
+        return (
+            <Box
+                as="section"
+                bg={{
+                    sm: { bg },
+                }}
+                shadow={{
+                    sm: 'base',
+                }}
+                overflow="hidden"
+                transition="all 0.2s"
+                _hover={{
+                    shadow: {
+                        sm: 'lg',
+                    },
+                }}
+            >
+                <Flex direction="column">
+                    <Link href={href}>
+                        <Img maxHeight="60" w={`100%`} objectFit="cover" alt={title} src={blogImage} />
+                    </Link>
+                    <Flex
+                        direction="column"
+                        justifyContent={`space-between`}
+                        px={{
+                            sm: '6',
+                        }}
+                        py="5"
+                    >
+                        <Box>
+                            <Heading as="h3" size="md" mb="1" lineHeight="base" color={themeGold}>
+                                <Link href={href}>{title}</Link>
+                            </Heading>
+                            <Text
+                                casing="uppercase"
+                                letterSpacing="wider"
+                                fontSize="xs"
+                                fontWeight="semibold"
+                                mb="3"
+                                color={themeBlue}
+                            >
+                                {/* {date} */}
+                                {moment(date).format("MMM Do YY")}
+                            </Text>
+                            <Box mb="3" dangerouslySetInnerHTML={{
+                                __html: description,
+                            }} />
+                        </Box>
+                        <Button
+                            as={`a`}
+                            href={href}
+                            bg={themeBlue}
+                            color={`white`}
+                            maxW={`40%`}
+                            alignSelf={`flex-end`}
+                            _hover={{ bg: `secondary`, }}
+                        >
+                            Read More...
+                        </Button>
+                    </Flex>
+                </Flex>
+            </Box>
+        )
+    }
+}
+
+export default BlogCard
--- src/templates/news.js
+++ src/templates/news.js
@@ -1,17 +1,44 @@
-import { Box, Grid } from "@chakra-ui/react"
+import { Box, Grid, Heading, useColorModeValue } from "@chakra-ui/react"
 import React from "react"
 import Layout from "../components/Layout/Layout"
+import BlogCard from "../components/BlogCard/index"
+import { repeat } from "lodash"
 
 const NewsPage = data => {
   const { articles } = data?.pageContext
+  const themeBlue = useColorModeValue(`primary`, `gray.300`)
   return (
     <Layout>
       <Box>
         <Grid>
-          <Box>
-            <h1>News</h1>
+          <Box mb={8} bg={`gray.100`}>
+            <Box maxW={`90%`} m={`auto`} py={8}>
+              <Heading as="h1" size="xl" mb="2" lineHeight="base" color={themeBlue}>
+                Blog
+              </Heading>
+              {articles.slice(0, 1).map(blog => (
+                <BlogCard
+                  featured={true}
+                  title={blog.title}
+                  href={blog.handle}
+                  description={blog.summary_html}
+                  date={blog.published_at}
+                  blogImage={blog.image.src}
+                />
+              ))}
+            </Box>
           </Box>
-          <Box></Box>
+          <Grid templateColumns={[`repeat(1, 1fr)`, `repeat(2, 1fr)`, `repeat(2, 1fr)`, `repeat(3, 1fr)`]} gridGap={8} maxW={`90%`} m={`auto`}>
+            {articles.slice(1, 20).map(blog => (
+              <BlogCard
+                title={blog.title}
+                href={blog.handle}
+                description={blog.summary_html}
+                date={blog.published_at}
+                blogImage={blog.image.src}
+              />
+            ))}
+          </Grid>
         </Grid>
       </Box>
     </Layout>
